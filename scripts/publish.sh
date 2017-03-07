1d4147299e9c79b3bb561538052a27316b654925
Joseph <fskreuz@gmail.com>
2017-03-06 19:06:13 -0500

    Remove scripts

--- scripts/publish.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-set -e
-
-git config --global user.name "Travis CI"
-git config --global user.email "fskreuz+travis@gmail.com"
-git remote add gh-remote "https://${GH_TOKEN}@github.com/ractivejs/ractivejs.github.io.git"
-git fetch gh-remote master:master
-mkdocs gh-deploy --clean --quiet --remote-name gh-remote --remote-branch master > /dev/null 2>&1
